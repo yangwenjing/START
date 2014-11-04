438c64179d92635168186a444ceb8201f4ff9f99
yangwenjing <yangwenjing246@gmail.com>
2014-11-04 16:40:45 +0800

    创建STARTMovement类

---
+++ movement/STARTMovement.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package movement;
+
+import core.Settings;
+
+/**
+ * @author Yang Wenjing
+ *
+ */
+public class STARTMovement extends ShortestPathMapBasedMovement {
+
+	/**
+	 * @param settings
+	 */
+	public STARTMovement(Settings settings) {
+		super(settings);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param mbm
+	 */
+	public STARTMovement(ShortestPathMapBasedMovement mbm) {
+		super(mbm);
+		// TODO Auto-generated constructor stub
+	}
+
+}
