f6f23700e7a4f2f0bf95a3b5d0762803cfc49bcf
Labi Kyo <labikyo@gmail.com>
2015-05-07 16:12:13 +0800

    完成场景的创建

--- movement/entity/ExtRegion.java
+++ movement/entity/ExtRegion.java
@@ -12,14 +12,23 @@ public class ExtRegion {
      * 参数区
      * regionid
      */
+    public String region_key;
     public int region_id;
     //一个region对应哪些格子
+    public int event;
     public Hashtable<String,ExtGrid> grids;
     public List<Integer> times;
 
-    public ExtRegion(int region_id)
+    public ExtRegion(int region_id,int event)
     {
+        this.region_key = getRegionKey(region_id,event);
         this.region_id = region_id;
+        this.event = event;
         this.grids = new Hashtable<String,ExtGrid>();
+    }
+
+    public static String getRegionKey(int region_id,int event)
+    {
+        return event+"-"+region_id;
     }
 }
--- movement/entity/Scene.java
+++ movement/entity/Scene.java
@@ -2,14 +2,10 @@ package movement.entity;
 
 import core.Settings;
 import core.SettingsError;
-import gui.InfoPanel;
-import movement.Cell;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Collections;
-import java.util.Hashtable;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * 一个单例模式
@@ -19,8 +15,7 @@ public class Scene {
     private static Scene ourInstance = null;
 
     public static Scene getInstance(Settings settings) {
-        if(ourInstance==null)
-        {
+        if (ourInstance == null) {
             return new Scene(settings);
         }
         return ourInstance;
@@ -29,28 +24,29 @@ public class Scene {
     /***************begion of 参数区*******************/
     /**
      * setttings parameters
-     *
      */
-    public static String[] Area_matrix_inputFileName;
+    public static String[][] Area_matrix_inputFileName;
     public static int beginTime;//开始时刻
     public static int timeLength;//仿真几小时
+    public static List<List<Integer>> timeList = new ArrayList<List<Integer>>();
 
     /**
      * grid的x,y长度
      */
-    public static double glen_x,glen_y;
+    public static double glen_x, glen_y;
     /**
      * grids x,y 方向的个数
      */
-    public static int grids_x,grids_y;
+    public static int grids_x, grids_y;
 
-    public Hashtable<String,ExtGrid> grids=null;
-    public Hashtable<Integer,ExtRegion> regionPool = new Hashtable<Integer,ExtRegion>();
-    public Hashtable<Integer,Hashtable<Integer,ExtRegion>>regionSets = null;//建立time－region的关系
+    public Hashtable<String, ExtGrid> grids = null;
+    public Hashtable<String, ExtRegion> regionPool = new Hashtable<String, ExtRegion>();
+    public Hashtable<String, Hashtable<String, ExtRegion>> timeEventRegionSets = null;//建立time－region的关系
+    public Hashtable<String, ExtRegion> timeGrid2Region = null;
 
     /***************end of 参数区*******************/
     /**
-     *初始化获取grid，region，regionset
+     * 初始化获取grid，region，regionset
      */
     private Scene(Settings settings) {
         //read settings
@@ -62,36 +58,34 @@ public class Scene {
     /**
      * 初始化Grid
      */
-    private void initGrid()
-    {
-        grids = new Hashtable<String,ExtGrid>();
+    private void initGrid() {
+        grids = new Hashtable<String, ExtGrid>();
 
-        for(int i=0;i<grids_x;i++)
-        {
-            for(int j=0;j<grids_y;j++)
-            {
-                ExtGrid grid = new ExtGrid(i,j);
-                grids.put(grid.grid_id,grid);
+        for (int i = 0; i < grids_x; i++) {
+            for (int j = 0; j < grids_y; j++) {
+                ExtGrid grid = new ExtGrid(i, j);
+                grids.put(grid.grid_id, grid);
             }
         }
     }
+
     /*
      * 初始化区域
      */
-    private void initRegions()
-    {
+    private void initRegions() {
+        loadGrid2Region2RegionSet(0);
+        loadGrid2Region2RegionSet(1);
 
     }
 
 
-    private void loadGrid2Region2RegionSet() {
-          // TODO Auto-generated method stub
+    private void loadGrid2Region2RegionSet(int event) {
+        // TODO Auto-generated method stub
         //time gain size 24
         int fileIndex = 0;
-        for(int i=0;i<fileIndex;i++)
-        {
-            File inFile = new File(Area_matrix_inputFileName[i]);
-            System.out.println("begin loading cells and region ["+Area_matrix_inputFileName[i]+"]...");
+        for (int i = 0; i < fileIndex; i++) {
+            File inFile = new File(Area_matrix_inputFileName[event][i]);
+            System.out.println("begin loading cells and region [" + Area_matrix_inputFileName[event][i] + "]...");
 
             Scanner scanner;
             try {
@@ -101,55 +95,68 @@ public class Scene {
                         "file " + inFile);
             }
 
+            Hashtable<String, ExtRegion> _regions = new Hashtable<String, ExtRegion>();
 
-            while(scanner.hasNextLine())
-            {
+            while (scanner.hasNextLine()) {
 
                 String nextLine = scanner.nextLine().trim();
 
                 String s[] = nextLine.split("\t");
-                if(s.length<4)continue;
+                if (s.length < 4) continue;
                 int x = Integer.parseInt(s[0]);
                 int y = Integer.parseInt(s[1]);
 
-//                int num = Integer.parseInt(s[2]);
                 int region_id = Integer.parseInt(s[3]);
-
                 String gridKey = ExtGrid.getKeyForGrid(x, y);
                 ExtGrid grid = this.grids.get(gridKey);
 
-                ExtRegion region = this.getRegionFromPool(region_id);
-                region.grids.put(gridKey,grid);
+                ExtRegion region = this.getRegionFromPool(region_id,event);
 
+                region.grids.put(gridKey, grid);
+                _regions.put(ExtRegion.getRegionKey(region_id,event), region);
 
-
-
-
-
-
+                /**
+                 * 建立反向索引
+                 * from time,grid to region
+                 */
+                for (int j = 0; j < (timeList.get(i)).size(); j++) {
+                    String tgKey = getTimeGridKey(timeList.get(i).get(j), gridKey);
+                    this.timeGrid2Region.put(tgKey, region);
+                }
 
             }
+
+            for (int j = 0; j < (timeList.get(i)).size(); j++) {
+                int _time = timeList.get(i).get(j);
+                String teKey = getTimeEventKey(_time,event);
+                this.timeEventRegionSets.put(teKey, _regions);
+            }
+
+            //建立索引，从time，grid，到regionId
+
+
             System.out.println("fininsh loading cells and region...");
             scanner.close();
-            Collections.sort(this.cells);
 
         }
 
 
-
-
-
-
     }
 
-    private ExtRegion getRegionFromPool(int region_id)
+    private ExtRegion getRegionFromPool(int region_id, int event) {
+        String region_key = ExtRegion.getRegionKey(region_id, event);
+        if (!this.regionPool.contains(region_key)) {
+            ExtRegion region = new ExtRegion(region_id, event);
+            this.regionPool.put(region_key, region);
+        }
+        return this.regionPool.get(region_key);
+    }
+    public static String getTimeEventKey(int time, int event)
     {
-        if(!this.regionPool.contains(region_id))
-        {
-            ExtRegion region = new ExtRegion(region_id);
-            this.regionPool.put(region_id,region);
-        }
-        return this.regionPool.get(region_id);
+        return time + "-" + event;
     }
 
+    public static String getTimeGridKey(int time, String gridKey) {
+        return time + "-" + gridKey;
+    }
 }
