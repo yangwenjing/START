cbb048852e11fe76cfdda63b714cca1ca7656dcf
Labi Kyo <labikyo@gmail.com>
2015-05-08 17:32:16 +0800

    去掉持续时长

--- movement/ExSTARTMovement.java
+++ movement/ExSTARTMovement.java
@@ -26,28 +26,13 @@ public class ExSTARTMovement extends MapBasedMovement implements SwitchableMovem
      * 记录节点的速度
      */
     private double speed;
-    /**
-     * 记录节点的持续时长
-     */
-//    private double duration;
+    
     /**
      * 引入仿真开始时刻
      */
     private int beginTime;
 
     /** 其他参数和配置不变   */
-    /**
-     * 状态0  设置持续时长的参数
-     */
-    private static double DURATION_A_FOR_STATUS0 = 0.971101;
-    private static double DURATION_PARA_FOR_STATUS0 = 0.00217593;
-
-    /**
-     * 状态1 的持续时长参数
-     */
-    private static double DURATION_A_FOR_STATUS1 = 0.988955;
-    private static double DURATION_PARA_FOR_STATUS1 = 0.00103644;
-
 
     public static final String TRANSITION_PROB_0 = "TransProbFile0";
     public static final String TRANSITION_PROB_1 = "TransProbFile1";
